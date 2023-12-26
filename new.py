c9069f0b378481892ce182264b984818209660bf
helllopratik <pratikgondane07@gmail.com>
2023-12-26 19:10:39 +0530

    added process thearding so that voice and video should work simulatenously

--- new.py
+++ new.py
@@ -2,12 +2,13 @@ import cv2
 from keras.models import model_from_json
 import numpy as np
 import speech_recognition as sr
-
-# Load the facial emotion detection model and cascade classifier...
-# Define functions for facial emotion detection...
+import threading
 
 # Initialize speech recognizer
 recognizer = sr.Recognizer()
+
+# Load the facial emotion detection model and cascade classifier...
+# Define functions for facial emotion detection...
 
 json_file = open("facialemotionmodel.json", "r")
 model_json = json_file.read()
@@ -22,29 +23,41 @@ def extract_features(image):
     feature = feature.reshape(1, 48, 48, 1)
     return feature / 255.0
 
-webcam = cv2.VideoCapture(0)
-labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+# Define a function for video capture
+def capture_video():
+    webcam = cv2.VideoCapture(0)
+    labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+    while True:
+        ret, im = webcam.read()
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(im, 1.3, 5)
+        
+        try:
+            for (p, q, r, s) in faces:
+                image = gray[q:q + s, p:p + r]
+                cv2.rectangle(im, (p, q), (p + r, q + s), (255, 0, 0), 2)
+                image = cv2.resize(image, (48, 48))
+                img = extract_features(image)
+                pred = model.predict(img)
+                prediction_label = labels[pred.argmax()]
+                print("Predicted Output:", prediction_label)
+                cv2.putText(im, '% s' % (prediction_label), (p - 10, q - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
+            
+            cv2.imshow("Output", im)
+            key = cv2.waitKey(1)
+            
+            if key == ord('q'):
+                break
+            
+        except cv2.error:
+            pass
 
-while True:
-    ret, im = webcam.read()
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(im, 1.3, 5)
-    
-    try:
-        for (p, q, r, s) in faces:
-            image = gray[q:q + s, p:p + r]
-            cv2.rectangle(im, (p, q), (p + r, q + s), (255, 0, 0), 2)
-            image = cv2.resize(image, (48, 48))
-            img = extract_features(image)
-            pred = model.predict(img)
-            prediction_label = labels[pred.argmax()]
-            print("Predicted Output:", prediction_label)
-            cv2.putText(im, '% s' % (prediction_label), (p - 10, q - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
-        
-        cv2.imshow("Output", im)
-        key = cv2.waitKey(1)
-        
-        # Speech recognition
+    webcam.release()
+    cv2.destroyAllWindows()
+
+# Define a function for speech recognition
+def recognize_speech():
+    while True:
         with sr.Microphone() as source:
             print("Say something:")
             audio = recognizer.listen(source)
@@ -60,13 +73,15 @@ while True:
             print("Could not understand audio")
         except sr.RequestError as e:
             print("Error: {0}".format(e))
-        
-        if key == ord('q'):
-            break
-        
-    except cv2.error:
-        pass
 
-webcam.release()
-cv2.destroyAllWindows()
+# Start video capture thread
+video_thread = threading.Thread(target=capture_video)
+video_thread.start()
 
+# Start speech recognition thread
+speech_thread = threading.Thread(target=recognize_speech)
+speech_thread.start()
+
+# Wait for threads to finish
+video_thread.join()
+speech_thread.join()
